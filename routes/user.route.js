2ed41767f48411bf98612d5ad9fe6665987edc18
Sarah Kastama <sarahkastama@gmail.com>
2021-10-18 15:41:22 -0700

    commit

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,6 +3,4 @@ const User = require('../controllers/user.controller');
 module.exports = app => {
     app.post('/api/user/register', User.register);
     app.post('/api/user/login', User.login);
-
-    app.get('/api/helloWorld', User.helloWorld)
 }
