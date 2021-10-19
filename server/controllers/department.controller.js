c04e1c9cb5f39f531e434fe857b4f9379d616755
Jiaxin(Peter) Liu <peterliu8368@gmail.com>
2021-10-19 10:52:41 -0700

    add controller functions

--- server/controllers/department.controller.js
+++ server/controllers/department.controller.js
@@ -9,6 +9,71 @@ modules.export.createDept = (req, res) => {
         });
 }
 
-//adding user / manager to a department
+//adding manager to a department
+modules.export.AddManagerToDept = (req, res) => {
+    Department.findByIdAndUpdate(req.body.deptId, 
+        {
+            $push: { managers: req.body.managerId }
+        })
+        .then(result => res.json(result))
+        .catch(err => {
+            res.status(400).json({ error: err });
+        });
+}
+//removing manager in a department
+modules.export.RemoveManagerFromDept = (req, res) => {
+    Department.findByIdAndUpdate(req.body.deptId, 
+        {
+            $pull: { managers: req.body.managerId }
+        })
+        .then(result => res.json(result))
+        .catch(err => {
+            res.status(400).json({ error: err });
+        });
+}
 
-//removing user / manager in a department
+//adding employee to a department
+modules.export.AddEmployeeToDept = (req, res) => {
+    Department.findByIdAndUpdate(req.body.deptId, 
+        {
+            $push: { employees: req.body.employeeId }
+        })
+        .then(result => res.json(result))
+        .catch(err => {
+            res.status(400).json({ error: err });
+        });
+}
+//removing employee in a department
+modules.export.RemoveEmployeeFromDept = (req, res) => {
+    Department.findByIdAndUpdate(req.body.deptId, 
+        {
+            $pull: { employees: req.body.employeeId }
+        })
+        .then(result => res.json(result))
+        .catch(err => {
+            res.status(400).json({ error: err });
+        });
+}
+
+//adding post to a department
+modules.export.AddPostToDept = (req, res) => {
+    Department.findByIdAndUpdate(req.body.deptId, 
+        {
+            $push: { posts: req.body.postId }
+        })
+        .then(result => res.json(result))
+        .catch(err => {
+            res.status(400).json({ error: err });
+        });
+}
+//removing post in a department
+modules.export.RemovePostFromDept = (req, res) => {
+    Department.findByIdAndUpdate(req.body.deptId, 
+        {
+            $pull: { posts: req.body.postId }
+        })
+        .then(result => res.json(result))
+        .catch(err => {
+            res.status(400).json({ error: err });
+        });
+}
