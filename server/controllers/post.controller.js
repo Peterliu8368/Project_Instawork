16814ad7f2628fd0f1c716bb26b4991cef533d42
Trevor Engen <48308407+trevorengen@users.noreply.github.com>
2021-10-19 11:10:33 -0700

    Add basic post controller and route.

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -1,9 +1,43 @@
 const Post = require('../models/post.model');
+const Department = require('../models/department.model');
 
 //create a post
+module.exports.createPost = (req, res) => {
+    Post.create(req.body)
+        .then(() => res.status(200).json('Success'))
+        .catch(err => res.status(400).json({ error: err }));
+}
 
 //get all posts from users who are in a certain department, filtered by date and privilege.
+//Req.body should contain an object in format: { department: depName }.
+module.exports.postsByDep = (req, res) => {
+    Department.find(req.body, 'posts')
+        .populate('posts')
+        .then(posts => res.status(200).json({ posts: posts }))
+        .catch(err => res.status(400).json({ error: err }));
+}
 
 //Get one post
+//Get post by post _id.
+//Structure req.body as { id: _postId }.
+module.exports.findPostById = (req, res) => {
+    Post.findById(req.body.id)
+        .then(post => res.status(200).json({ post: post }))
+        .catch(err => res.status(400).json({ error: err }));
+}
+//Get posts by user _id.
+//Req.body should be in format of { userId: _userId }.
+module.exports.findPostsByUserId = (req, res) => {
+    Post.find(req.body)
+        .then(post => res.status(200).json({ post: post }))
+        .catch(err => res.status(400).json({ error: err }));
+}
 
 //edit a post
+//Update post by postId.
+//Structure req.body as following { postId: _postId, (optional)postText: newText, (optional)reviewMessage: newMessage, (optional)workResult: newResult }.
+module.exports.updatePostFromId = (req, res) => {
+    Post.findByIdAndUpdate(req.body.postId, req.body, { new: true })
+        .then(post => res.status(200).json({ updatedPost: post }))
+        .catch(err => res.status(400).json({ error: err }));
+}
--- server/routes/post.route.js
+++ server/routes/post.route.js
@@ -1,5 +1,17 @@
 const Post = require('../controllers/post.controller');
 
 module.exports = app => {
+    //create
+    app.post('/api/post/create', Post.createPost);
+
+    //retrieve
+    app.post('/api/post/department', Post.postsByDep);
+    app.post('/api/post/userid', Post.findPostsByUserId);
+    app.post('/api/post/postid', Post.findPostById);
+
+    //update
+    app.put('/api/post/updateById', Post.updatePostFromId);
+
+    //delete
 
 }
