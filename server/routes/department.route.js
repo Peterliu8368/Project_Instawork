f9d222d44aea89fa0876a794fd976b64b1045cd5
Jiaxin(Peter) Liu <peterliu8368@gmail.com>
2021-10-19 12:00:41 -0700

    adding department api routes

--- server/routes/department.route.js
+++ server/routes/department.route.js
@@ -1,5 +1,17 @@
 const Department = require('../controllers/department.controller');
 
 module.exports = app => {
-    app.post("/api/department/create", Department.createDept);
+    app.post('/api/department/create', Department.createDept);
+    app.put('/api/department/employee/add', Department.AddEmployeeToDept);
+    app.put('/api/department/employee/remove', Department.RemoveEmployeeFromDept);
+    
+    app.put('/api/department/manager/add', Department.AddManagerToDept);
+    app.put('/api/department/manager/remove', Department.RemoveManagerFromDept);
+
+    app.put('/api/department/post/add', Department.AddPostToDept);
+    app.put('/api/department/post/remove', Department.RemovePostFromDept);
+
+    app.delete('/api/department/remove', Department.removeDept);
+
+    app.get('/api/department/post/getAll', Department.GetAllPostsFromDepartment);
 }
