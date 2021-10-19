615e824e18171a6cb441d2cd30a78b0e32939a1e
Sarah Kastama <sarahkastama@gmail.com>
2021-10-19 11:41:28 -0700

    commit

--- server/controllers/department.controller.js
+++ server/controllers/department.controller.js
@@ -1,7 +1,14 @@
 const Department = require('../models/department.model');
 
 //create a department
-
+module.exports.createDept = (req, res) => {
+    Department.create(req.body)
+        .then(newDept => res.json(newDept))
+        .catch(err => {
+            res.status(400).json({ error: err });
+        });
+    
+}
 //adding user / manager to a department
 
 //removing user / manager in a department
--- server/controllers/organization.controller.js
+++ server/controllers/organization.controller.js
@@ -1,7 +1,44 @@
 const Organization = require('../models/organization.model');
 
-//create an organization
+module.exports = {
+    //create an organization
+    createOrg(req, res) {
+        // const userToken = jwt.sign({ id: user._id }, JWT_SECRET);
+        Organization.create(req.body)
+            .then(org => res.json(org))
+            .catch(err => res.status(400).json(err))
+    },
 
-//adding department in an organization
+    //adding department in an organization
+    addDepartment(req, res) {
+        Organization.findByIdAndUpdate(
+            {_id:req.body.orgId}, 
+            {
+                $push: { departments: req.body.deptId }
+            },
+            { new: true }
+        )
+            .then(org => res.json(org))
+            .catch(err => res.status(400).json(err))
+    },
 
-//removing department in an organization
+    //removing department in an organization
+    deleteDepartment(req, res) {
+        Organization.findByIdAndUpdate(
+            {_id:req.body.orgId},
+            {
+                $pull: {
+                    departments: { _id: req.body.deptId }
+                }
+            },
+            { multi: true }
+        )
+            .then(deleteDep => res.json(deleteDep))
+            .catch(err => res.status(400).json(err))
+    }
+}
+
+
+
+
+
