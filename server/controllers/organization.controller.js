fde6b193ac28067378e1d71446f1f8e40e411751
Jiaxin(Peter) Liu <peterliu8368@gmail.com>
2021-10-19 08:49:13 -0700

    add comments for controllers needed

--- server/controllers/organization.controller.js
+++ server/controllers/organization.controller.js
@@ -1,1 +1,7 @@
 const Organization = require('../models/organization.model');
+
+//create an organization
+
+//adding department in an organization
+
+//removing department in an organization
--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -1,1 +1,9 @@
 const Post = require('../models/post.model');
+
+//create a post
+
+//get all posts from users who are in a certain department, filtered by date and privilege.
+
+//Get one post
+
+//edit a post
