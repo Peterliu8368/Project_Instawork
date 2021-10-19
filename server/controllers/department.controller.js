5bfa270da7ae5fd3b6f4f08d30a9104f17b4648f
Jiaxin(Peter) Liu <peterliu8368@gmail.com>
2021-10-19 10:17:36 -0700

    adding create function

--- server/controllers/department.controller.js
+++ server/controllers/department.controller.js
@@ -1,6 +1,13 @@
-const Department = require('../models/department.model');
+const Department = require("../models/department.model");
 
 //create a department
+modules.export.createDept = (req, res) => {
+    Department.create(req.body)
+        .then(newDept => res.json(newDept))
+        .catch(err => {
+            res.status(400).json({ error: err });
+        });
+}
 
 //adding user / manager to a department
 
