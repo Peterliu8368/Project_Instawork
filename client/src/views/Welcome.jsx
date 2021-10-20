90493532d51b151483433e66dd5f67780b6438ea
Jiaxin(Peter) Liu <peterliu8368@gmail.com>
2021-10-20 09:59:55 -0700

    welcome page

--- client/src/views/Welcome.jsx
+++ client/src/views/Welcome.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
+
 import { useState } from 'react';
 import { useHistory } from 'react-router-dom'
 import { Container, Grid, Card, Paper, Typography } from '@mui/material';
 import axios from 'axios';
+import {UserContext} from '../App';
+import React, {useContext} from 'react';
 
 const Welcome = () => {
+
+    const {state, dispatch} = useContext(UserContext);
 
     return (
         <>
             <Paper style={{margin: "20px auto", width: "80vw", height: "80vh", padding: "20px"}}  elevation={3}>
-                    <Typography style={{textAlign: 'center', marginTop: '20px'}} variant='h3'>Welcome (Add username)</Typography>
+                    <Typography style={{textAlign: 'center', marginTop: '20px'}} variant='h3'>Welcome {state.firstName}</Typography>
 
             </Paper>
         </>
