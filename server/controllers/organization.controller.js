65e44e590aefa80b25a753c99122c0c0542b15d3
Trevor Engen <48308407+trevorengen@users.noreply.github.com>
2021-10-20 16:36:08 -0700

    Add controller to get all depts in org.

--- server/controllers/organization.controller.js
+++ server/controllers/organization.controller.js
@@ -27,6 +27,13 @@ module.exports.addAdmin = (req, res) => {
         });
 }
 
+module.exports.getAllDepts = (req, res) => {
+    Organization.findById(req.body.orgId)
+        .populate('departments')
+        .then(result => res.status(200).json(result.departments))
+        .catch(err => res.status(400).json(err));
+}
+
 
 
 
