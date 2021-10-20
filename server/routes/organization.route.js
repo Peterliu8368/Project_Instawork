72b6e48f240d1a73a1d8d17a77a667fda84c2c24
Trevor Engen <48308407+trevorengen@users.noreply.github.com>
2021-10-20 16:36:01 -0700

    Add route to get all depts in org.

--- server/routes/organization.route.js
+++ server/routes/organization.route.js
@@ -2,5 +2,7 @@ const Organization = require('../controllers/organization.controller');
 
 module.exports = app => {
     app.post("/api/organization/create", Organization.createOrg);
-    app.post("/api/organization/addAdmin", Organization.addAdmin)
+    app.post("/api/organization/addAdmin", Organization.addAdmin);
+
+    app.get('/api/organization/depts', Organization.getAllDepts);
 }
